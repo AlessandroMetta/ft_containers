449d0ae6abeda7d1a52ab1b16c70357983e35f3f
AlessandroMetta <ametta@student.42roma.it>
2022-03-08 13:01:47 +0100

    tester update

--- vector/tester/test.sh
+++ vector/tester/test.sh
@@ -1,15 +1,15 @@
 #------------ VARIABLES ----------------
-TEST_RED="\033[1;31m"
-TEST_GREEN="\033[1;32m"
-TEST_DEFAULT="\033[0m"
+TEXT_COLOR_RED="\033[1;31m"
+TEXT_COLOR_GREEN="\033[1;32m"
+TEXT_COLOR_DEFAULT="\033[0m"
 COMPILE_FLAGS="clang++ -Wall -Wextra -Werror -std=c++98 -pedantic"
 
 TEST_NAME=(\
-    "constructors" \
+    # "constructors" \
     "= operator" \
     "at" \
     "[] operator" \
-    "assign" \
+    # "assign" \
     "front/back" \
     "size, max_size and capacity" \
     "resize" \
@@ -19,40 +19,39 @@ TEST_NAME=(\
     "clear" \
     )
 
+how_many=${#TEST_NAME[@]}
+
 STD_SRCS=(\
-    std_test_constr.cpp \
-    std_test_=.cpp \
-    std_test_at.cpp \
-    std_test_[].cpp \
-    std_test_assign.cpp \
-    std_test_front_back.cpp \
-    std_test_ms_s_c.cpp \
-    std_test_resize.cpp \
-    std_test_reserve.cpp \
-    std_test_pb.cpp \
-    std_test_swap.cpp \
-    std_test_clear.cpp \
+    # "std_test_constr.cpp" \
+    "std_test_=.cpp" \
+    "std_test_at.cpp" \
+    "std_test_[].cpp" \
+    # "std_test_assign.cpp" \
+    "std_test_front_back.cpp" \
+    "std_test_ms_s_c.cpp" \
+    "std_test_resize.cpp" \
+    "std_test_reserve.cpp" \
+    "std_test_pb.cpp" \
+    "std_test_swap.cpp" \
+    "std_test_clear.cpp" \
     )
 
 FT_SRCS=(\
-    ft_test_constr.cpp \
-    ft_test_=.cpp \
-    ft_test_at.cpp \
-    ft_test_[].cpp \
-    ft_test_assign.cpp \
-    ft_test_front_back.cpp \
-    ft_test_ms_s_c.cpp \
-    ft_test_resize.cpp \
-    ft_test_reserve.cpp \
-    ft_test_pb.cpp \
-    ft_test_swap.cpp \
-    ft_test_clear.cpp \
+    # "ft_test_constr.cpp" \
+    "ft_test_=.cpp" \
+    "ft_test_at.cpp" \
+    "ft_test_[].cpp" \
+    # "ft_test_assign.cpp" \
+    "ft_test_front_back.cpp" \
+    "ft_test_ms_s_c.cpp" \
+    "ft_test_resize.cpp" \
+    "ft_test_reserve.cpp" \
+    "ft_test_pb.cpp" \
+    "ft_test_swap.cpp" \
+    "ft_test_clear.cpp" \
     )
-    
-how_many=${#TEST_NAME[@]}
 
-
-#------------ START HERE ----------------
+#------------ PROGRAM START HERE ----------------
 
 echo "-------------------------"
 echo "|     VECTOR TESTER     |"
@@ -65,9 +64,9 @@ do
     # leaks -atExit -- ./a.out | grep "total leaked bytes."
     ${COMPILE_FLAGS} ${STD_SRCS[$i]} ; ./a.out > std.o
     if cmp -s yours.o std.o; then
-        echo "${TEST_GREEN}OK${TEST_DEFAULT}"
+        echo "${TEXT_COLOR_GREEN}OK${TEXT_COLOR_DEFAULT}"
     else
-       echo "${TEST_RED}KO${TEST_DEFAULT}"
+       echo "${TEXT_COLOR_RED}KO${TEXT_COLOR_DEFAULT}"
     fi
 done
 
